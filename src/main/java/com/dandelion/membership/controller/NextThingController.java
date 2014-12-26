51f6ae1fee6447076277a1ab313924c2286eff0c
Shaman <wow2009zfx@gmail.com>
2014-12-26 18:14:15 +0800

    Add thing database model

---
+++ src/main/java/com/dandelion/membership/controller/NextThingController.java
@@ -0,0 +1,11 @@
+package com.dandelion.membership.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by Shaman on 12/26/14.
+ */
+@Controller
+public class NextThingController {
+
+}
